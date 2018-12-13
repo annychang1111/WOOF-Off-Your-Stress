03ada24850f7ff1e19c8c56c9c5619ed4b967d9a
annychang1111 <30671162+annychang1111@users.noreply.github.com>
2018-12-12 19:25:27 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 // var isMobile = 0;
 var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+var socket = io('https://woof-off-your-stress.herokuapp.com/');
 
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
